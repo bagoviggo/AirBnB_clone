fcff196161180acefa52fe30a9a3847a73782e37
maiyo008 <tonymaiyo008@gmail.com>
2023-02-09 21:30:14 +0000

    Create class file that serializes/deserialize

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@
 """Defines a  base class for all the other classes in this project"""
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel():
@@ -20,12 +21,13 @@ class BaseModel():
                 updated_at (datetime): current datetime an instance is updated
         """
         if kwargs is not None:
-            self.id = kwargs.get('id', "default1")
+            self.id = kwargs.get('id')
             self.created_at = kwargs.get('created_at')
             self.updated_at = kwargs.get('updated_at')
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        storage.new(self)
 
     def __str__(self):
         """Returns a string that prints the format
@@ -40,19 +42,19 @@ class BaseModel():
         """
 
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all
         keys/values of __dict__ of the instance
         """
-
-        dict_a = self.__dict__
-        class_model = {
-                '__class__': self.__class__.__name__,
-                'created_at': self.created_at.isoformat(),
-                'updated_at': self.updated_at.isoformat()
-                }
-        dict_a.update(class_model)
+        dict_a = {}
+        for key, value in self.__dict__.items():
+            setattr(self, key, value)
+            dict_a[key] = value
+        dict_a["__class__"] = self.__class__.__name__
+        dict_a["created_at"] = self.created_at.isoformat()
+        dict_a["updated_at"] = self.updated_at.isoformat()
         return (dict_a)
 
     @classmethod
@@ -72,5 +74,6 @@ class BaseModel():
 
         # Return the instance
         return cls(**dict_a)
+    
 
 
