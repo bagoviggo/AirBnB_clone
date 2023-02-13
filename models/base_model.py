67bf5a75dd9f2fc9b5c3f3c0d35535054ab9975b
bagoviggo <bagoviggo@gmail.com>
2023-02-13 23:06:15 +0300

    Change base_model

--- models/base_model.py
+++ models/base_model.py
@@ -21,18 +21,14 @@ class BaseModel():
                 updated_at (datetime): current datetime an instance is updated
         """
 
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-                elif key != '__class__':
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-
+        if kwargs is not None:
+            self.id = kwargs.get('id')
+            self.created_at = kwargs.get('created_at')
+            self.updated_at = kwargs.get('updated_at')
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        storage.new(self)
 
     def __str__(self):
         """Returns a string that prints the format
